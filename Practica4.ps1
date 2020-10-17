8f3328d497b365dec5f54a985c5f7d9feebb9357
EduardMunz <linehacking412@gmail.com>
2020-10-17 16:15:18 -0500

    Powershell

---
+++ Practica4.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem 
+ForEach ($Item in "Get-ChildItem")
+{
+Format-Table | Outfile listado.txt
+} 
+Select-String "Documents" listado.txt | out-file resultado.txt
+Clear-Host 
+Get-Content resultado.txt
