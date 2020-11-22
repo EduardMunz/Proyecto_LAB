24621d4f76875a7365c3ce17e0b6e5da175aa4c7
EduardMunz <linehacking412@gmail.com>
2020-11-22 14:04:23 -0600

    Practica 8 de FTP

--- Practica8.py
+++ Practica8.py
@@ -1,12 +1,18 @@
-from ftplib import FTP_FTP_PORT
-import os
+#Eduardo Tadeo Muñoz Macías
+#!/usr/bin/python3
+RUTA_SERVIDOR_FTP = 'ftp.us.debian.org'
+import ftplib
+def cliente_ftp_conexion(servidor, nombre_usuario, correo):
+    #hacemos la apertura de la conexion
+    ftp = ftplib.FTP(servidor, nombre_usuario, correo)
+    #listamos los archivos del directorio /pub
+    #ftp.cwd("/pub")
+    print ("Archivos disponibles en %s:" %servidor)
+    archivos = ftp.dir()
+    print (archivos)
+    ftp.retrbinary('RETR welcome.msg', open ('welcome.msg', 'wb').write)
+    #file.close()
+    ftp.quit()
 
-ftp = FTP('ftp.us.debian.org')
-ftp.login()
-os.system('mkdir carpeta_TXT')
-
-for line in ftp.terlines('LIST') > tempo.txt:
-    if line.startwith{'-r'} ==1:
-        os.system(line="cv carpeta_TXT")
-
-ftp.close()
+if __name__ == '__main__':
+    cliente_ftp_conexion(servidor=RUTA_SERVIDOR_FTP, nombre_usuario='anonymous',correo='nobody@nourl.com', )
